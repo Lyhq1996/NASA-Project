39e395647ef793063f60230d7eaf6bd97c4bebca
unknown <lihongqian_shufe@163.com>
2022-08-27 22:13:13 -0400

    connect POST/launches with frontend dashboard and implement DELETE/launches

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,6 +1,8 @@
 const { 
     getAllLaunches,
-    addNewLaunch 
+    addNewLaunch,
+    existsLaunchWithId,
+    abortLaunchById,
 } = require('../../models/launches.model');
 
 function httpGetAllLaunches(req, res) {
@@ -17,20 +19,41 @@ function httpAddNewLaunch(req, res) {
     }
 
     launch.launchDate = new Date(launch.launchDate);
-
     if (isNaN(launch.launchDate)) {
         return res.status(400).json({
           error: 'Invalid launch date',
         });
     }
 
-
-
     addNewLaunch(launch);
     return res.status(201).json(launch);
 }
 
+function httpAbortLaunch(req, res) {
+    const launchId = Number(req.params.id);
+  
+    const existsLaunch = existsLaunchWithId(launchId);
+    if (!existsLaunch) {
+      return res.status(404).json({
+        error: 'Launch not found',
+      });
+    }
+  
+    const aborted = abortLaunchById(launchId);
+    if (!aborted) {
+      return res.status(400).json({
+        error: 'Launch not aborted',
+      });
+    }
+  
+    return res.status(200).json({
+      ok: true,
+    });
+}
+  
 module.exports = {
     httpGetAllLaunches,
     httpAddNewLaunch,
-}
+    httpAbortLaunch,
+};
+
