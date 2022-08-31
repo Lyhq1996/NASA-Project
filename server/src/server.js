59ec34e83dedad0646d822eba9fc0b62c4244f9e
unknown <lihongqian_shufe@163.com>
2022-08-31 19:15:09 -0400

    use SpaceX api to populate historical launches

--- server/src/server.js
+++ server/src/server.js
@@ -1,33 +1,24 @@
 const http = require('http');
-const mongoose = require('mongoose');
 
-const app = require('./app'); 
+require('dotenv').config();
 
+const app = require('./app');
+const { mongoConnect } = require('./services/mongo');
 const { loadPlanetsData } = require('./models/planets.model');
+const { loadLaunchData } = require('./models/launches.model');
 
 const PORT = process.env.PORT || 8000;
 
-const MONGO_URL = 'mongodb+srv://nasa-api:US5TOI2MohfTAbKB@nasacluster.6pojqds.mongodb.net/nasa?retryWrites=true&w=majority';
-
 const server = http.createServer(app);
 
-mongoose.connection.once('open', () => {
-    console.log('MongoDB connection ready!');
-});
-
-mongoose.connection.on('error', (err) => {
-    console.error(err);
-});
-
 async function startServer() {
-    await mongoose.connect(MONGO_URL);
-    await loadPlanetsData();
-    server.listen(PORT, () => {
-        console.log(`Listening on port ${PORT}`);
-    });
+  await mongoConnect();
+  await loadPlanetsData();
+  await loadLaunchData();
+  
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
 }
- 
+
 startServer();
-
-
-
